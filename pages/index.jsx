ce05dd433550abaa4dc71b72ec302984abf3c7f6
FahadAbdulQayyum <fahad.45843@iqra.edu.pk>
2023-07-30 23:03:26 +0500

    tailwind css setup made

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,27 @@
 import Link from "next/link";
 
-const Index = ({data}) => {
-    console.log('data',data);
-    const products = data.map(p => <div><Link href={`/product/${p.id}`}>{p.title}</Link></div>)
-  return (
-    <div>{products}</div>
-  )
-}
+const Index = ({ data }) => {
+  console.log("data", data);
+  const products = data.map((p) => {
+    return (
+    <>
+      <img src={p.thumbnail}/>
+      <Link href={`/product/${p.id}`}>{p.title}</Link>
+    </>
+    )
+  });
+  return <div>{products}</div>;
+};
 
 export const getStaticProps = async () => {
-    const data = await fetch('https://dummyjson.com/products')
-    const res = await data.json();
+  const data = await fetch("https://dummyjson.com/products");
+  const res = await data.json();
 
-    return {
-        props: {
-            data: res.products
-        }
-    }
-}
+  return {
+    props: {
+      data: res.products,
+    },
+  };
+};
 
-export default Index
+export default Index;
